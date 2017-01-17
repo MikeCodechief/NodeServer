049f09838d6e555780f0e29d0808dbdb4ea49b28
Mike Lo <mike.hermes@me.com>
2017-01-16 18:32:43 -0700

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req);
   res.render('index', { title: 'NODE Express' });
 });
-router.post('/', function(req,res){
-	console.log("This is POST");
+router.post('/', function(req,res,next){
+  var user_id = req.body.id;
+  var token = req.body.token;
+  //res.send(user_id + " " + token);
+  res.send("Data Recivied");
+	console.log("POST: " + user_id + " " + token);
 });
 module.exports = router;
